4444e314e2eaa18e8585ad7fa72f1bd96c13aea7
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-07 13:57:51 +1300

    Update for new dependency based build.

--- infusion.rb
+++ infusion.rb
@@ -3,16 +3,22 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.4"
-require 'teapot/build'
+required_version "0.5"
 
-define_package "unit-test-0.2.0" do |package|
-	package.build(:all) do |platform, environment|
-		top = Teapot::Build.top(Pathname.new(__FILE__).dirname)
+define_package "unit-test" do |package|
+	package.install do |environment|
+		top = Teapot::Build.top(package.path)
 
 		top.add_directory('source')
 		top.add_directory('test')
 		
 		top.execute(:install, environment)
+	end
+	
+	package.depends :system
+	package.depends "Language/C++11"
+	
+	package.provides "Library/unit-test" do
+		append linkflags "-lUnitTest"
 	end
 end
