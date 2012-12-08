5c08c3b12deda8f4482da3bec6deeeb368a6b9ac
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2012-12-08 19:58:56 +1300

    Use simplified Teapot::Build.install_directory.

--- teapot.rb
+++ teapot.rb
@@ -7,11 +7,7 @@ required_version "0.5"
 
 define_target "unit-test" do |target|
 	target.install do |environment|
-		top = Teapot::Build.top(package.path)
-
-		top.add_directory('source')
-
-		top.execute(:install, environment)
+		Teapot::Build.install_directory(package.path, 'source', environment)
 	end
 	
 	target.depends :platform
@@ -24,11 +20,7 @@ end
 
 define_target "unit-test-tests" do |target|
 	target.install do |environment|
-		top = Teapot::Build.top(package.path)
-
-		top.add_directory('test')
-		
-		top.execute(:install, environment)
+		Teapot::Build.install_directory(package.path, 'test', environment)
 	end
 	
 	target.depends :platform
