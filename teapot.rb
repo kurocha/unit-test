5248a3770a869bf5db5417dec281c4222dc64775
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-05-07 19:24:40 +1200

    Bumped version and add run configuration for travis.

--- teapot.rb
+++ teapot.rb
@@ -3,7 +3,7 @@
 #  This file is part of the "Teapot" project, and is released under the MIT license.
 #
 
-required_version "0.7.0"
+required_version "0.8.0"
 
 define_target "unit-test" do |target|
 	target.build do |environment|
@@ -23,9 +23,23 @@ define_target "unit-test-tests" do |target|
 		build_directory(package.path, 'test', environment)
 	end
 	
+	target.run do |environment|
+		environment = environment.flatten
+		
+		Commands.run(environment[:install_prefix] + "bin/unit-tests-test-runner")
+	end
+	
 	target.depends :platform
 	target.depends "Language/C++11"
 	target.depends "Library/UnitTest"
 	
 	target.provides "Test/UnitTest"
+end
+
+define_configuration "travis" do |configuration|
+	configuration[:source] = "https://github.com/dream-framework"
+	
+	configuration.import! "platforms"
+	
+	configuration[:run] = ["Test/UnitTest"]
 end
